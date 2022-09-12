b5eb87a96b7f73034e670eb9e5054d418eb1a622
SeanKeane1 <seankean36@gmail.com>
2022-09-11 23:28:50 -0400

    added buttons and DOMSelectors

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const DOMSelectors = {
     searchBar: document.getElementsByClassName(".search-bar"),
-}
+    favBtn: document.getElementsByClassName(".favorite-btn"),
+    container: document.getElementsByClassName(".container")
+};
+
+// need to access JSON data
+
+
+
+// need a function to display everything
+
+
+
+// need a function to display certain results based on search data
+
+
+
+// need a function to distinguish favorites
+
 
