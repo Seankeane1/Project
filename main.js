b281de8f77f5f576b2b0e998cc5c3d6402610d97
Seankeane1 <108425807+Seankeane1@users.noreply.github.com>
2022-09-14 12:30:24 -0400

    class 9/14

--- main.js
+++ main.js
@@ -4,36 +4,45 @@ const DOMSelectors = {
     container: document.querySelectorAll(".container")
 };
 
+let contacts = [];
+
+DOMSelectors.searchBar.addEventListener('input', function(e){
+
+const searchValue = e.target.value.toLowerCase(); // lowercase makes it easier to fiter without being specific
+const filteredResult = contacts // filter output
+})
+
 // need to access JSON data
 
-const searchStates = async searchText => {
+const searchContacts = async searchText => {
     const response = await fetch('../data/contacts.json');
-    const contacts = await response.json();
+    contacts = await response.json();
 
+    console.log(Array.isArray(contacts));
+    console.log(contacts[0].name)
 
-      let content = DOMSelectors.container.innerHTML = `<div class="contact-info">
-    <h1 id="name">${contacts[0].name}</h1>
-    <h2 id="last-name">${contacts[0].name}</h2>
+    // need a function to display everything
+
+const displayContacts = (people) => {
+
+}
+
+/* let content = DOMSelectors.container.innerHTML = `<div class="contact-info">
+    <h1 id="name">${contacts.name}</h1>
+    <h2 id="last-name">${contacts.name}</h2>
     <div class="favorite"></div>
     <div class="info">
-        <h3 class="phone-num">7949748765386</h3>
+        <h3 class="phone-num">7949748765</h3>
         <h3 class="email">filler1@gmail</h3>
     </div>`;
  
-    
-    //console.log(Array.isArray(contacts));
- 
-    console.log(contacts[0].name)
+     */
+
+// need a function to display certain results based on search data
 
 };
 
-searchStates();
-
-// need a function to display everything
-
-
-
-// need a function to display certain results based on search data
+searchContacts();
 
 
 
@@ -44,3 +53,5 @@ DOMSelectors.favBtn.addEventListener('click', () => {
 });
 
 // POSSIBLE* Toggle favorite function
+
+// POSSIBLE* Alphabetize contacts
