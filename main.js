9e1764a4908ee2e7bc4a2870f67c494fb5973665
SeanKeane1 <seankean36@gmail.com>
2022-09-15 22:10:04 -0400

    conflicting functions

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const DOMSelectors = {
 
 let contacts = [];
 
-DOMSelectors.searchBar.addEventListener('input', function(e){
+// need a function to display certain results based on search data
+DOMSelectors.searchBar.addEventListener('keyup', function(e){
 
-const searchValue = e.target.value.toLowerCase(); // lowercase makes it easier to fiter without being specific
+const searchValue = e.target.value.toLowerCase(); // lowercase makes it easier to filter without being specific
 const filteredResults = contacts.filter((contact) => {
     contact.name.toLowerCase().includes(searchValue) ||
-    contact.email.toLowerCase().includes(searchValue) ||
-    contact.phoneNum().includes(searchValue)
-}); // filter output
+    contact.email.toLowerCase().includes(searchValue)
+}); 
 displayContacts(filteredResults);
 });
 
@@ -23,32 +23,27 @@ const searchContacts = async searchText => {
     const response = await fetch('../data/contacts.json');
     contacts = await response.json();
 
-    //console.log(Array.isArray(contacts));
+    console.log(Array.isArray(contacts));
     //console.log(contacts[0].name)
-
-    // need a function to display everything
-
-const displayContacts = (people) => {
-    const outputHtml = people
-}
-
-/* let content = DOMSelectors.container.innerHTML = `<div class="contact-info">
-    <h1 id="name">${contacts.name}</h1>
-    <h2 id="last-name">${contacts.name}</h2>
-    <div class="favorite"></div>
-    <div class="info">
-        <h3 class="phone-num">7949748765</h3>
-        <h3 class="email">filler1@gmail</h3>
-    </div>`;
- 
-     */
-
-// need a function to display certain results based on search data
-
 };
 
-searchContacts();
+// need a function to display everything
+const displayContacts = (contacts) => {
+    const outputHtml = contacts.map((contact) => {
+        return `<div class="contact-info">
+        <h1 id="name">${contact.name}</h1>
+        <h2 id="last-name">${contact[0].name}</h2>
+        <div class="favorite"></div>
+        <div class="info">
+            <h3 class="phone-num">7949748765</h3>
+            <h3 class="email">filler1@gmail</h3>
+        </div>`;
+    }).join('');
+DOMSelectors.container.innerHTML = outputHtml;
+}
 
+
+searchContacts();
 
 
 // need a function to distinguish favorites
@@ -56,6 +51,8 @@ searchContacts();
 DOMSelectors.favBtn.addEventListener('click', () => {
     console.log('wack');
 });
+
+searchText();
 
 // POSSIBLE* Toggle favorite function
 
