86c9b36ffc9dc4fb8a62d3383fbb05284dd164fd
SeanKeane1 <seankean36@gmail.com>
2022-09-17 00:40:55 -0400

    problems
    Search data exists separately from the display function which is why it creates more underneath the original for every input, Listener event needs to be changed and the display needs to be altered to get smaller not larger

--- main.js
+++ main.js
@@ -7,17 +7,19 @@ const DOMSelectors = {
 let contacts = [];
 
 // need a function to display certain results based on search data
-DOMSelectors.searchBar.addEventListener('keyup', function(e){
+//DOMSelectors.searchBar.addEventListener('keyup', searchInput)
 
-const searchValue = e.target.value.toLowerCase(); // lowercase makes it easier to filter without being specific
-const filteredResults = contacts.filter((contact) => {
+const searchInput = function(e){
+
+    const searchValue = e.target.value.toLowerCase(); // lowercase makes it easier to filter without being specific
+    const filteredResults = contacts.filter((contact) => {
     return (
     contact.name.toLowerCase().includes(searchValue) //||
     //contact.email.toLowerCase().includes(searchValue)
     );
 }); 
 displayContacts(filteredResults);
-});
+};
 
 // need to access JSON data
 
@@ -34,13 +36,11 @@ const alphabetize = (prop) =>
 contacts.sort(alphabetize(`name`));
 
 displayContacts(contacts);
-
-
-    //console.log(Array.isArray(contacts));
-    //console.log(contacts[1].name);
     };
 
-     const displayContacts = () => contacts.forEach(contact => {
+//display each contact prior to search
+    
+    const displayContacts = () => contacts.forEach(contact => {
         let outputHtml = `<div class="contact-info">
                     <h1 id="name">${contact.name}</h1>
                     <h2 id="last-name">${contact.lastName}</h2>
@@ -55,28 +55,12 @@ displayContacts(contacts);
 
        displayContacts(contacts);
  
+       DOMSelectors.searchBar.addEventListener('keyup', searchInput)
 
-// need a function to display everything
-
- /* if (searchValue.length === 0) {
-        const displayContacts = (contacts) => {
-            const outputHtml = contacts.map((contact) => {
-                return `<div class="contact-info">
-                <h1 id="name">${contact.name}</h1>
-                <h2 id="last-name">${contact[0].name}</h2>
-                <div class="favorite"></div>
-                <div class="info">
-                    <h3 class="phone-num">7949748765</h3>
-                    <h3 class="email">filler1@gmail</h3>
-                </div>`;
-            }).join('');
-        DOMSelectors.container.innerHTML = outputHtml;
-        }
-        
-    };  */
+//if (searchValue.length === 0) 
 
 
-loadContacts();
+loadContacts(displayContacts());
 
 
 // need a function to distinguish favorites
@@ -87,6 +71,3 @@ DOMSelectors.favBtn.addEventListener('click', () => {
 
 // POSSIBLE* Toggle favorite function
 
-
-
-// POSSIBLE* Alphabetize contacts
